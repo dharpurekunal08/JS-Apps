84feb4750930d5a7759c2c66e5dd94bbea9cca30
Kunal Dharpure <146354402+dharpurekunal08@users.noreply.github.com>
2024-06-02 08:44:53 +0000

    basics

---
+++ 01_Variable&DataTypes/first.js
@@ -0,0 +1,46 @@
+// console.log("Hare Krishna!");
+// console.log("Welcome to JavaScript!");
+
+
+// variable 
+let name = "Kunal";
+let age = 30;
+let height = '170cm';
+console.log(name, age, height);
+
+var g = 40;
+var g = 80;
+console.log(g);
+
+const pi = 3.14;
+console.log(pi);
+
+let x = BigInt('123');
+console.log(typeof x);
+
+const student = {
+    fname : "kunal",
+    lname : "Dharpure",
+    age : 20,
+    gpa : 3.34,
+    isPass : true
+}
+
+student.age += 1;
+console.log(student['fname']);
+console.log(student.lname);
+console.log(student['age'])
+console.log(typeof student);
+
+const product = {
+    title: "ball pen",
+    ratting: 4,
+    offer: 5,
+    price: 349,
+}
+
+console.log("abc" + 1);
+console.log(1 + "abc");
+
+
+
